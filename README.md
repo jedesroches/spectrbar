7be5592a04214b26cfef26a25967fa301292bec6
Joachim Desroches <jo@thedesroches.net>
2023-11-16 18:27:19 +0100

    Create battery section.

---
+++ README.md
@@ -0,0 +1,1 @@
+Small program to fill in the information I want in my status bar. See `spectrwm(1)` for the context.
---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::{fmt::Display, slice::IterMut};
+
+use battery::{units::ratio::percent, Battery};
+
+/*
+    The whole error management thing is a bit weird, since for once we actually
+    want to silently ignore errors: it's ok if some things aren't here, just
+    print the rest...
+
+    XXX somehow less flexible / safe than the shell script, since removing a
+    battery won't be detected
+
+    TODO while this is called from the script, it should be a oneshot, but
+    ideally I would like to update different parts of the bar at different intervals:
+
+    - reprint a line every second to get accurate time from strftime(3)
+    - reload battery only once every minute
+    - ...
+*/
+
+fn main() -> Result<(), battery::Error> {
+    let manager = battery::Manager::new()?;
+    let mut my_batteries: Vec<Battery> = manager.batteries()?.flatten().collect();
+
+    println!("{}", update_battery(&manager, my_batteries.iter_mut()));
+    Ok(())
+}
+
+#[derive(Debug)]
+struct BatteryStatus {
+    charging: bool,
+    charges: Vec<f32>,
+}
+
+impl Display for BatteryStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if !self.charging && self.charges.is_empty() {
+            return Ok(());
+        }
+
+        if self.charging {
+            f.write_str("⚇ ")?;
+        }
+
+        for charge in &self.charges {
+            write!(f, "{:.0} ", charge)?;
+        }
+
+        f.write_str("| ")?;
+
+        Ok(())
+    }
+}
+
+fn update_battery(manager: &battery::Manager, batteries: IterMut<Battery>) -> BatteryStatus {
+    let mut charging: bool = false;
+    let mut charges: Vec<f32> = Vec::new();
+
+    for battery in batteries {
+        match manager.refresh(battery) {
+            Ok(()) => {
+                match battery.state() {
+                    battery::State::Charging => charging = true,
+                    _ => (),
+                }
+                charges.push(battery.state_of_charge().get::<percent>())
+            }
+            Err(_) => (),
+        }
+    }
+    BatteryStatus { charging, charges }
+}
